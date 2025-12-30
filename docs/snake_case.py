716d8b7fd45cb1fdca61ac563d5c2156f9dc00b0
noumanshah436 <noumanshah436@gmail.com>
2025-12-31 00:07:55 +0500

    Self attention in transformers

--- docs/snake_case.py
+++ docs/snake_case.py
@@ -11,8 +11,10 @@ def to_snake_case(text):
     return text.lower()
 
  
-word_with_spaces = "day70-Bahdanau Attention Vs Luong Attention"
+word_with_spaces = "day73-Self Attention in Transformers "
 snake_case_word_with_spaces = to_snake_case(word_with_spaces)
 print(f"Original: {word_with_spaces}")
 print(f"Snake Case: {snake_case_word_with_spaces}")
  
+
+#   python docs/snake_case.py
